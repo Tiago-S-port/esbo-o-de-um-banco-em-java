83e92ab7eea5deba12f56804a8870099dcaf43f1
Tiago <tiagodesouzam@protonmail.com>
2026-05-05 20:01:14 -0300

    Update GerenciaBanco.java

--- GerenciaBanco.java
+++ GerenciaBanco.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.gerenciabanco;
 import java.util.Scanner;
 
-
-/**
- *
- * @author tiago
- */
  
 public class GerenciaBanco {
 
